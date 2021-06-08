f217bb42225c0cca2efbfafcac45fb00f5460d49
Antonello Di Nunzio <plugin@antonellodinunzio.online>
2021-06-08 22:07:39 +0200

    feat(report): Add save as csv to room report example

--- ironpython-scripts/report/pollination_report_room_properties.py
+++ ironpython-scripts/report/pollination_report_room_properties.py
@@ -28,6 +28,8 @@ import Share.Convert as CO # It contains utilities to convert RhinoObject <> HB 
 from System.Collections.Generic import List
 
 try:  # import honeybee dependencies
+    import io
+    import csv
     import json
     import honeybee.dictutil as hb_dict_util
     from honeybee.room import Room
@@ -61,55 +63,81 @@ class RoomGridView(forms.Dialog[bool]):
     def __init__(self, data):
         unit = str(doc_unit).lower()
         
+        self._data = data
         self.Title = "Room report"
         self.Resizable = True
         self.m_gridview = forms.GridView()
         self.m_gridview.ShowHeader = True
         
         self.m_gridview.DataStore = data
-
+        
+        self._header = ('display_name', 'floor_area [{}2]'.format(unit),
+        'volume [{}3]'.format(unit), 'exposed_area [{}2]'.format(unit),
+        'exterior_wall_aperture_area [{}2]'.format(unit), 
+        'exterior_wall_area [{}2]'.format(unit))
+        
         column1 = forms.GridColumn()
-        column1.HeaderText = 'display_name'
+        column1.HeaderText = self._header[0]
         column1.Editable = True
         column1.DataCell = forms.TextBoxCell(0)
         self.m_gridview.Columns.Add(column1)
 
         column2 = forms.GridColumn()
-        column2.HeaderText = 'floor_area [{}2]'.format(unit)
+        column2.HeaderText = self._header[1]
         column2.Editable = True
         column2.DataCell = forms.TextBoxCell(1)
         self.m_gridview.Columns.Add(column2)
 
         column3 = forms.GridColumn()
-        column3.HeaderText = 'volume [{}3]'.format(unit)
+        column3.HeaderText = self._header[2]
         column3.Editable = True
         column3.DataCell = forms.TextBoxCell(2)
         self.m_gridview.Columns.Add(column3)
 
         column4 = forms.GridColumn()
-        column4.HeaderText = 'exposed_area [{}2]'.format(unit)
+        column4.HeaderText = self._header[3]
         column4.Editable = True
         column4.DataCell = forms.TextBoxCell(3)
         self.m_gridview.Columns.Add(column4)
         
         column5 = forms.GridColumn()
-        column5.HeaderText = 'exterior_wall_aperture_area [{}2]'.format(unit)
+        column5.HeaderText = self._header[4]
         column5.Editable = True
         column5.DataCell = forms.TextBoxCell(4)
         self.m_gridview.Columns.Add(column5)
         
         column6 = forms.GridColumn()
-        column6.HeaderText = 'exterior_wall_area [{}2]'.format(unit)
+        column6.HeaderText = self._header[5]
         column6.Editable = True
         column6.DataCell = forms.TextBoxCell(5)
         self.m_gridview.Columns.Add(column6)
+        
+        self.m_button = forms.Button(self.OnSaveButton)
+        self.m_button.Text = 'Save File'
         
         layout = forms.DynamicLayout()
         layout.Padding = drawing.Padding(10)
         layout.Spacing = drawing.Size(5, 5)
         layout.Add(self.m_gridview)
+        layout.Add(self.m_button)
+        
+        self.save_dialog = forms.SaveFileDialog()
+        self.save_dialog.Title = 'Save File As'
+        self.save_dialog.FileName = '{}.csv'.format(Rhino.RhinoDoc.ActiveDoc.Name)
         
         self.Content = layout
+    
+    def OnSaveButton(self, s, e):
+        result = self.save_dialog.ShowDialog(self.m_gridview)
+        
+        # write csv ironpython 2
+        if result == forms.DialogResult.Ok:
+            with io.open(self.save_dialog.FileName, 'w', newline='') as file:
+                writer = csv.writer(file, delimiter=',')
+                writer.writerow(self._header)
+                writer.writerows(data)
+            
+            forms.MessageBox.Show(self, "Done!", self.Title);
 
 # create the dataset
 data = []
